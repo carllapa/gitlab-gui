9453619e00bbeddc4b46ef4fea8dbe29bdb34949
itxaka Serrano <itxakaserrano@gmail.com>
2013-10-08 22:23:47 +0200

    first version

---
+++ gitlab-gui.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Itxaka Serrano 2/08/13
+
+import logging
+
+
+# logging parameters below
+logging.basicConfig(filename='gitlab-gui.log', level=logging.DEBUG, format='%(asctime)s - %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
+logging.info("Program gitlab-gui Start")
+
+from sys import argv
+from PyQt4 import QtCore, QtGui
+import mainwindow
+#import gittle
+import ConfigParser
+from os import getenv
+import requests
+import json
+import markdown
+
+
+class mainwindow(QtGui.QMainWindow, mainwindow.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(mainwindow, self).__init__(parent)
+        self.setupUi(self)
+        self.label.hide()
+        self.label_2.hide()
+        self.label_3.hide()
+        self.label_4.hide()
+        self.label_5.hide()
+        self.actionExit.triggered.connect(self.close)
+        self.user = getenv("LOGNAME")  # get the logged username so we can configure git
+        self.label_login.setText("Logged as " + self.user)
+        self.connect(self.label_login, QtCore.SIGNAL("clicked()"),
+                     self.clickled_on_login)  # have to do this because we are clicking on a label
+        self.token = ""
+        self.host = ""
+        self.model = QtGui.QStandardItemModel(self.listView)
+        config = ConfigParser.ConfigParser()
+        config.read("config.ini")
+        try:
+            self.token = config.get("gitlab", "token")
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            QtGui.QMessageBox.information(self.parent(), "Authentication",
+                                          "We need to use your token in order to login to your gitlab instance\n"
+                                          "Please find it in your Gitlab profile", QtGui.QMessageBox.Ok)
+            while self.token == "":
+                self.token, ok = QtGui.QInputDialog().getText(self, "Token", "Enter your token:")
+            config.add_section("gitlab")
+            config.set("gitlab", "token", self.token)
+            with open("config.ini", "wb") as f:
+                config.write(f)
+        try:
+            self.host = config.get("gitlab", "url")
+        except ConfigParser.NoOptionError:
+            while self.host == "":
+                self.host, ok = QtGui.QInputDialog().getText(self, "Gitlab",
+                                                             "Enter the gitlab full URL (http://xxx.xxxx.xxx)")
+            config.set("gitlab", "url", self.host)
+            with open("config.ini", "wb") as f:
+                config.write(f)
+
+        project_url = self.host + "/api/v3/projects?private_token=" + self.token
+        r = requests.get(project_url)
+        self.repos = []
+        for repo in json.loads(r.content):
+            self.repos.append([repo['name'], repo['last_activity_at'], repo["web_url"]])
+            item = QtGui.QStandardItem()
+            item.setText(repo['name'])
+            self.model.appendRow(item)
+        self.listView.setModel(self.model)
+        self.listView.clicked.connect(self.on_item_changed)
+
+        # future use
+        #user_url = "http://localhost/api/v3/user?private_token=" + self.token
+        #r = requests.get(user_url)
+        #print json.loads(r.content)
+        #new_ssh_key_url = "http://localhost/api/v3/user/keys?private_token=" + self.token + "&title=" +
+        #  ssh_title + "&key=" + ssh_key
+
+    def on_item_changed(self, item):
+        r = requests.get(self.repos[item.row()][2] + "/raw/master/README?private_token=" + self.token)
+        if "<!DOCTYPE html>" in r.content:  # the 404 is html while the raw is plain text
+            text = "<p>There isn't a README for this repository</p>"
+            pass
+        else:
+            text = markdown.markdown(r.content)
+        self.label_info.setText("Last modified at: " + self.repos[item.row()][1] + "<br><br>" + text)
+
+    def clickled_on_login(self):
+        # Here we should put some popup to remove the token and logout
+        pass
+
+
+app = QtGui.QApplication(argv)
+form = mainwindow()
+form.show()
+app.exec_()
---
+++ mainwindow.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created: Fri Aug  2 14:42:06 2013
+#      by: PyQt4 UI code generator 4.10.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+
+class ExtendedQLabel(QtGui.QLabel):  # extend Qlabel to add a click event
+
+    def __init(self, parent):
+        QtGui.QLabel.__init__(self, parent)
+
+    def mouseReleaseEvent(self, ev):
+        self.emit(QtCore.SIGNAL('clicked()'))
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(959, 711)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.horizontalLayout.addWidget(self.label)
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.horizontalLayout.addWidget(self.label_2)
+        self.label_3 = QtGui.QLabel(self.centralwidget)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.horizontalLayout.addWidget(self.label_3)
+        self.label_4 = QtGui.QLabel(self.centralwidget)
+        self.label_4.setObjectName(_fromUtf8("label_4"))
+        self.horizontalLayout.addWidget(self.label_4)
+        self.label_5 = QtGui.QLabel(self.centralwidget)
+        self.label_5.setObjectName(_fromUtf8("label_5"))
+        self.horizontalLayout.addWidget(self.label_5)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.label_login = ExtendedQLabel(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_login.sizePolicy().hasHeightForWidth())
+        self.label_login.setSizePolicy(sizePolicy)
+        self.label_login.setFrameShape(QtGui.QFrame.Box)
+        self.label_login.setText(_fromUtf8(""))
+        self.label_login.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label_login.setObjectName(_fromUtf8("label_login"))
+        self.horizontalLayout.addWidget(self.label_login)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.listView = QtGui.QListView(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listView.sizePolicy().hasHeightForWidth())
+        self.listView.setSizePolicy(sizePolicy)
+        self.listView.setFrameShape(QtGui.QFrame.Box)
+        self.listView.setFrameShadow(QtGui.QFrame.Plain)
+        self.listView.setProperty("showDropIndicator", True)
+        self.listView.setUniformItemSizes(True)
+        self.listView.setObjectName(_fromUtf8("listView"))
+        self.horizontalLayout_2.addWidget(self.listView)
+        self.label_info = QtGui.QLabel(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_info.sizePolicy().hasHeightForWidth())
+        self.label_info.setSizePolicy(sizePolicy)
+        self.label_info.setFrameShape(QtGui.QFrame.NoFrame)
+        self.label_info.setText(_fromUtf8(""))
+        self.label_info.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label_info.setObjectName(_fromUtf8("label_info"))
+        self.horizontalLayout_2.addWidget(self.label_info)
+        self.gridLayout.addLayout(self.horizontalLayout_2, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 959, 20))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuMenu = QtGui.QMenu(self.menubar)
+        self.menuMenu.setObjectName(_fromUtf8("menuMenu"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionPreferences = QtGui.QAction(MainWindow)
+        self.actionPreferences.setObjectName(_fromUtf8("actionPreferences"))
+        self.actionExit = QtGui.QAction(MainWindow)
+        self.actionExit.setObjectName(_fromUtf8("actionExit"))
+        self.menuMenu.addAction(self.actionPreferences)
+        self.menuMenu.addSeparator()
+        self.menuMenu.addAction(self.actionExit)
+        self.menubar.addAction(self.menuMenu.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "gitlab-gui", None))
+        self.label.setText(_translate("MainWindow", "TextLabel", None))
+        self.label_2.setText(_translate("MainWindow", "TextLabel", None))
+        self.label_3.setText(_translate("MainWindow", "TextLabel", None))
+        self.label_4.setText(_translate("MainWindow", "TextLabel", None))
+        self.label_5.setText(_translate("MainWindow", "TextLabel", None))
+        self.menuMenu.setTitle(_translate("MainWindow", "Menu", None))
+        self.actionPreferences.setText(_translate("MainWindow", "Preferences", None))
+        self.actionExit.setText(_translate("MainWindow", "Exit", None))
+
